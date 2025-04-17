32511422c35bc0faabdac3809a2722fb9695a52a
guoyingxu <guoyx@itshixun.com>
2025-04-17 15:24:51 +0800

    增加自动更新代码

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
+const update = await check();
+if (update) {
+  await update.downloadAndInstall();
+  await relaunch();
+} else {
+  createApp(App).mount("#app");
+}
