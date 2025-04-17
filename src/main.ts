1969dd0e7b32d4452c20c6ce20d6fd202bcfd0b8
guoyingxu <guoyx@itshixun.com>
2025-04-17 15:33:03 +0800

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import App from "./App.vue";
 
 import { check } from "@tauri-apps/plugin-updater";
 import { relaunch } from "@tauri-apps/plugin-process";
-const update = await check();
-if (update) {
-  await update.downloadAndInstall();
-  await relaunch();
-} else {
-  createApp(App).mount("#app");
+async function checkVersion() {
+  const update = await check();
+  if (update) {
+    await update.downloadAndInstall();
+    await relaunch();
+  } else {
+    createApp(App).mount("#app");
+  }
 }
+checkVersion();
