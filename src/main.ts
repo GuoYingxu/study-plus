31fb188114fc05120d69871bff17820bdd1cdecf
guoyingxu <guoyx@itshixun.com>
2025-04-17 16:52:34 +0800

    fixbug版本号检查

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import App from "./App.vue";
 import { check } from "@tauri-apps/plugin-updater";
 import { relaunch } from "@tauri-apps/plugin-process";
 async function checkVersion() {
-  const update = await check();
-  if (update) {
-    await update.downloadAndInstall();
-    await relaunch();
-  } else {
+  try {
+    const update = await check();
+    if (update) {
+      await update.downloadAndInstall();
+      await relaunch();
+    } else {
+      createApp(App).mount("#app");
+    }
+  } catch (e) {
+    console.log("版本检查失败：直接启动", e);
     createApp(App).mount("#app");
   }
 }
