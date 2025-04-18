0d8bf4bf76dbbec80594ec27650733109b73bad0
guoyingxu <guoyx@itshixun.com>
2025-04-18 08:58:18 +0800

    v0.1.24

--- src/App.vue
+++ src/App.vue
@@ -20,16 +20,9 @@ const onClick = () => {
   <main class="container">
     <h1>Welcome to Study +</h1>
 
+    <!-- 描述-->
     <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+      Study + 是一个基于 Vue3 + Tauri 的桌面应用程序，用于学习和练习编程。
     </div>
 
     <div>edupluc inc.</div>
